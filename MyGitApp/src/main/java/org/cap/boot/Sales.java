21bfb051bbcc0d646da12ad52709346c37b1c0ff
caprepo <vidavid@LIN73000499.corp.capgemini.com>
2017-08-08 12:33:35 +0530

    display method updated.

--- MyGitApp/src/main/java/org/cap/boot/Sales.java
+++ MyGitApp/src/main/java/org/cap/boot/Sales.java
@@ -1,12 +1,13 @@
 package org.cap.boot;
 
 public class Sales {
+	int[] arr={12,34,56,78,0};
 	
 	public void display(){
 		System.out.println("Display Sum");
 		int sum=0;
-		for(int i=0;i<100;i++)
-			sum+=i;
+		for(int i=0;i<arr.length;i++)
+			sum+=arr[i];
 	}
 	
 	public void show(){
